b1f5e0c550ae916600a0ef3e5bba8500f8084e30
Jhony Henao <196105668+kanlahac@users.noreply.github.com>
2026-02-12 10:22:03 -0500

    [Add] Nav component - Config MUI

---
+++ componentes/ui/nav/nav.list.ts
@@ -0,0 +1,6 @@
+export const NAV_LIST: (string | { name: string; href: string })[] = [
+
+    "Charts",
+    { name: "MUI-X", href: "/mui-x" },
+    
+]
---
+++ componentes/ui/nav/nav.tsx
@@ -0,0 +1,36 @@
+"use client";  
+
+import Link from "next/link";
+import { Divider, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import { NAV_LIST } from "./nav.list";
+import { usePathname } from "next/navigation";
+
+export default function Nav() {
+    const pathname = usePathname();
+
+    return (
+        <List>
+            {NAV_LIST.map((item, index) => { 
+
+                if (typeof item === "string") {
+                    return (
+                        <Divider key={index} textAlign="left" sx={{ my: 2, '&::before, &::after': { borderColor: 'divider' } }}>
+                            <Typography variant="caption" sx={{ color: 'text.secondary', fontWeight: 'bold', textTransform: 'uppercase' }}>
+                                { item }
+                            </Typography>
+                        </Divider>
+                    );
+                }
+
+                return (
+                    <ListItem key={index} disablePadding>
+                        <ListItemButton component={Link} href={item.href} selected={pathname === item.href}>
+                            <ListItemText primary={item.name} />
+                        </ListItemButton>
+                    </ListItem>
+                )
+
+            })}
+        </List>
+    );
+}
